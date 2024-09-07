16b33155bdd049faa1d9bdc6ec37ca435066a4e3
eduardoSantiag0 <143814420+eduardoSantiag0@users.noreply.github.com>
2024-09-07 18:16:08 -0300

    feat: Add Flyway migrations and use ResponseEntity as return values

--- backend/sistema-inscricao-de-materias/src/main/java/br/com/sistemas/sistema_inscricao_de_materias/controllers/AlunoController.java
+++ backend/sistema-inscricao-de-materias/src/main/java/br/com/sistemas/sistema_inscricao_de_materias/controllers/AlunoController.java
@@ -6,6 +6,8 @@ import br.com.sistemas.sistema_inscricao_de_materias.model.Materia;
 import br.com.sistemas.sistema_inscricao_de_materias.service.AlunoService;
 import br.com.sistemas.sistema_inscricao_de_materias.service.MateriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +24,7 @@ public class AlunoController {
 
 
     @PostMapping("/add")
-    public String add(@RequestBody AlunoDTO alunoDTO) {
+    public ResponseEntity<String> add(@RequestBody AlunoDTO alunoDTO) {
         try {
             System.out.println(alunoDTO);
 
@@ -34,9 +36,10 @@ public class AlunoController {
             Aluno aluno = converterAluno(alunoDTO, materiasInscritas);
 
             alunoService.salvarAluno(aluno);
-            return "Novo aluno adicionado";
+            return ResponseEntity.status(HttpStatus.CREATED).body("Novo aluno adicionado");
+
         } catch (Exception e) {
-            return "Erro ao adicionar aluno: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao adicionar aluno: " + e.getMessage());
         }
     }
 
@@ -47,24 +50,30 @@ public class AlunoController {
     }
 
    @GetMapping("/find")
-    public List<AlunoDTO> findAlunos () {
-       List<Aluno> listaAlunos =  alunoService.findAll();
+    public ResponseEntity<List<AlunoDTO>> findAlunos () {
+        try {
+            List<Aluno> listaAlunos =  alunoService.findAll();
 
 
-       List<String> listaMaterias = listaAlunos.stream()
-               .flatMap(a -> a.getMatriasInscritas().stream())
-               .map(Materia::getNomeMateria)
-               .collect(Collectors.toList());
+            List<String> listaMaterias = listaAlunos.stream()
+                    .flatMap(a -> a.getMatriasInscritas().stream())
+                    .map(Materia::getNomeMateria)
+                    .collect(Collectors.toList());
 
-       List<AlunoDTO> listaDTOs = listaAlunos.stream()
-               .map(aluno -> new AlunoDTO(
-                       aluno.getNomeCompleto(),
-                       aluno.getNotaGeral(),
-                       aluno.getAnoIngressao().getYear(),
-                       listaMaterias
-               ))
-               .collect(Collectors.toList());
-       return listaDTOs;
+            List<AlunoDTO> listaDTOs = listaAlunos.stream()
+                    .map(aluno -> new AlunoDTO(
+                            aluno.getNomeCompleto(),
+                            aluno.getNotaGeral(),
+                            aluno.getAnoIngressao().getYear(),
+                            listaMaterias
+                    ))
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(listaDTOs);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+
    }
 
 }
---
+++ backend/sistema-inscricao-de-materias/src/main/resources/db/migration/V2__populate_tables.sql
@@ -0,0 +1,35 @@
+INSERT INTO
+    materia (capacidade_maxima, nome_materia, tem_espaco)
+VALUES
+    (12, 'Montagem', true),
+    (25, 'História do Cinema', true),
+    (10, 'Animação', true),
+    (15, 'Som', true),
+    (20, 'Semiótica', true),
+    (18, 'Roteiro', true),
+    (30, 'Cinematografia', true);
+
+
+INSERT INTO
+    aluno (ano_ingressao, nome_completo, nota_geral, prioridade)
+VALUES
+    ('2019-01-01', 'Jason Voorhees', 7820, false),
+    ('2020-01-01', 'Michael Myers', 9200, true),
+    ('2021-01-01', 'Freddy Krueger', 6400, false),
+    ('2022-01-01', 'Leatherface', 5600, true),
+    ('2023-01-01', 'Ghostface', 8900, false),
+    ('2020-01-01', 'Chucky', 4750, false),
+    ('2021-01-01', 'Pennywise', 9950, true),
+    ('2019-01-01', 'Hannibal Lecter', 7200, false),
+    ('2020-01-01', 'Candyman', 5100, true),
+    ('2021-01-01', 'Jigsaw', 8300, false),
+    ('2022-01-01', 'Norman Bates', 6850, true),
+    ('2023-01-01', 'Patrick Bateman', 8000, false),
+    ('2021-01-01', 'Carrie White', 6900, true),
+    ('2019-01-01', 'Annie Wilkes', 7500, false),
+    ('2022-01-01', 'The Babadook', 6150, false),
+    ('2023-01-01', 'Samara Morgan', 7050, true),
+    ('2020-01-01', 'Jack Torrance', 9000, true),
+    ('2021-01-01', 'The Creeper', 5300, false),
+    ('2019-01-01', 'Pinhead', 8700, true),
+    ('2023-01-01', 'Victor Crowley', 4600, false);
